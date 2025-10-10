8dd357be200e38f6b4700466396c46701e32969f
PsyEnc X <wertstracys@gmail.com>
2025-10-10 12:19:31 -0400

    update lalo.py

---
+++ lalo.py
@@ -0,0 +1,28 @@
+
+valor_numero = int(input("Asere, ¿cuánto le metiste a cada número?: "))
+cantidad_jugadas = int(input("¿Cuántas jugaditas te tiraste hoy, socio?: "))
+total_numeros = int(input("¿Cuántos números jugaste hoy, asere?: "))
+
+paga_fijo = input("¿Pagan a 85 por cada peso, o no? (si/no): ")
+if paga_fijo.lower() == "si":
+    premio_acierto = 85 * valor_numero
+else:
+    pago_por_peso = int(input("¿A cómo está el pago por peso hoy, asere?: "))
+    premio_acierto = pago_por_peso * valor_numero
+
+veces_ganadas = int(input("¿Y cuántas veces te coronaste hoy, campeón?: "))
+
+gasto_por_jugada = total_numeros * valor_numero
+gasto_total = gasto_por_jugada * cantidad_jugadas
+ganancia_total = veces_ganadas * premio_acierto
+ganancia_neta = ganancia_total - gasto_total
+
+print("Se te fue este billete en el juego: ", gasto_total, "💸")
+
+if veces_ganadas > 0:
+    print("Pero mira lo que entró por los aciertos: ", ganancia_total, "🤑")
+
+if ganancia_neta >= 0:
+    print("¡Epa! ¡Tremenda ganancia neta hoy!: ", ganancia_neta, "🎉\n¡Te coronaste, socio!")
+else:
+    print("¡Ay, mi madre! ¡Tremenda tumbada!\nPerdiste: ", abs(ganancia_neta), "😭 ¡Pa' la próxima será!")
