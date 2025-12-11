17521d834956dfcc531f3689d23055ddb76a0767
PsyEnc X <wertstracys@gmail.com>
2025-12-11 11:53:40 -0500

    update config.py

--- bot/config.py
+++ bot/config.py
@@ -50,7 +50,7 @@ class Config:
             self.FCODEC = config("FCODEC", default=None)
             self.FFMPEG = config(
                 "FFMPEG",
-                default='ab-av1 auto-encode -i "{}" --enc map=-0:v:1? --enc map=-0:t? --acodec libopus --enc fps_mode=passthrough --enc b:a=64k --scd true --svt tile-columns=0:scm=0:film-grain=0:color-primaries=1:transfer-characteristics=1:matrix-coefficients=1:luminance-qp-bias=20 --keyint 193 --min-vmaf 89 --preset 6 --max-crf 70 --min-samples 3 --sample-every 12m --sample-duration 20s --vfilter scale=1280:-2:flags=bicubic:param0=0:param1=1/2 --downmix-to-stereo -o "{}"',
+                default='ab-av1 auto-encode -i "{}" --enc map=-0:v:1? --enc map=-0:t? --acodec libopus --enc b:a=64k --scd true --svt tile-columns=0:scm=0:film-grain=0:color-primaries=1:transfer-characteristics=1:matrix-coefficients=1:luminance-qp-bias=20 --keyint 193 --min-vmaf 91 --preset 5 --max-crf 70 --min-samples 3 --sample-every 12m --vfilter scale=1280:-2:flags=bicubic:param0=0:param1=1/2 --downmix-to-stereo -o "{}"',
             )
             self.FFMPEG2 = config("FFMPEG2", default=None)
             self.FFMPEG3 = config("FFMPEG3", default=None)
