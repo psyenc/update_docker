c201c5af6175c679ca3ee8cbff5134e99bc98437
PsyEnc X <wertstracys@gmail.com>
2026-02-11 07:05:47 -0500

    update config.py

--- bot/config.py
+++ bot/config.py
@@ -50,7 +50,7 @@ class Config:
             self.FCODEC = config("FCODEC", default=None)
             self.FFMPEG = config(
                 "FFMPEG",
-                default='ab-av1 auto-encode -i "{}" --enc map=-0:v:1? --enc map=-0:t? --acodec libopus --enc b:a=64k --scd true --svt tile-columns=0:enable-dlf=2:enable-dg=0:mbr-overshoot-pct=0:scm=0:tune=0:color-primaries=1:transfer-characteristics=1:matrix-coefficients=1:qm-min=0:qm-max=15:chroma-qm-min=0:variance-boost-strength=1:variance-octile=4:tf-strength=1:luminance-qp-bias=10:sharpness=1:ac-bias=1.5:sharp-tx=1:hbd-mds=1:complex-hvs=1:noise-adaptive-filtering=0 --keyint 193 --min-vmaf 91 --preset 4 --max-crf 70 --min-samples 3 --vfilter scale=1280:-2:flags=bicubic:param0=0:param1=1/2 --downmix-to-stereo -o "{}"',
+                default='ab-av1 auto-encode -i "{}" --enc map=-0:v:1? --enc map=-0:t? --acodec libopus --enc b:a=64k --scd true --svt tile-columns=0:enable-dlf=2:scm=0:color-primaries=1:transfer-characteristics=1:matrix-coefficients=1:tf-strength=1:luminance-qp-bias=10:sharpness=1 --keyint 193 --min-vmaf 91 --preset 4 --max-crf 70 --min-samples 3 --vfilter scale=1280:-2:flags=bicubic:param0=0:param1=1/2 --downmix-to-stereo -o "{}"',
             )
             self.FFMPEG2 = config("FFMPEG2", default=None)
             self.FFMPEG3 = config("FFMPEG3", default=None)
